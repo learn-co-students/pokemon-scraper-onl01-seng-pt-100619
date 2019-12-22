b64b784373f24cf3811f72a2d343c125ea7bd716
allison cortez <allizetorc@gmail.com>
2019-12-22 19:44:56 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,14 +8,11 @@ class Pokemon
     @db = db
   end
   
-  def save
-    sql = <<-SQL
-      INSERT INTO pokemon (name, type) 
-      VALUES (?, ?)
-    SQL
- 
-    DB[:conn].execute(sql, self.name, self.type)
- 
-  end
+  # def self.save(name,type,db)
+  #   db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  # end
+
   
+    
+
 end
